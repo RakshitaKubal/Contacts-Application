a8f7ee609d1e2d204937fd57d5977e9f316470a8
PavitraDavaluri <150511588+PavitraDavaluri@users.noreply.github.com>
2023-11-13 12:16:32 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,3 +1,14 @@
+#initialize an empty contacts dictionary to store contact information
+contacts = {}
+
+# function to add a new contact
+def add_contact():
+    name = input("Enter the name:")
+    phone = input("Enter the phone number:")
+    email = input("Enter the email:")
+    contacts[name] = {"Phone": phone, "Email": email}
+    print(f"contact '{name}' added successfully!")
+
 # Function to view contact details
 def view_contact(name):
     if name in contacts:
